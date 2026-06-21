1c02c97119fb16372e6bae9e3635d14507d2681e
smruti <csmruti30@gmail.com>
2026-06-21 11:46:49 +0530

    Update api.ts

--- sprout-frontend/services/api.ts
+++ sprout-frontend/services/api.ts
@@ -4,4 +4,14 @@ const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default API;
